fe79e3d03654b753b3619db1d716c26674f9bb0d
ElhamJafari <elhamjafari200312@gmail.com>
2025-07-06 15:10:50 +0330

    feat: To read html and css files

---
+++ server.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const querystring = require("querystring");
+const bcrypt = require("bcrypt");
+const { Food, User, Reserve, Course, Request, News, Finance } = require("./db");
+
+// To read html and css files
+function serveFile(filePath, contentType, res) {
+	fs.readFile(filePath, (err, data) => {
+		if (err) {
+			res.writeHead(500, { "Content-Type": "text/plain" });
+			res.end("Internal Server Error");
+		} else {
+			res.writeHead(200, { "Content-Type": contentType });
+			res.end(data);
+		}
+	});
+}
